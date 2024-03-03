7937a84f9594287b1b36d3c63f0042c4ecc5830b
Bamberek <kabaczek99@gmail.com>
2024-03-03 01:52:03 +0100

    added employee data options

---
+++ src/data/employeesOptions.js
@@ -0,0 +1,18 @@
+import avatar1 from "../assets/employee1.png";
+import avatar2 from "../assets/employee2.png";
+
+const employeeDescription =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+export const employees = [
+  {
+    image: avatar1,
+    name: "Imię Nazwisko [ Dział 1]",
+    description: employeeDescription,
+  },
+  {
+    image: avatar2,
+    name: "Imię Nazwisko [ Dział 2]",
+    description: employeeDescription,
+  },
+];
